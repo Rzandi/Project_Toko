7ae14199a7a80cdafcf6afe4bfbff73efc594a9b
Developer <dev@invoiceease.local>
2025-11-16 17:34:16 +0700

    edit backend .en

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: envPath })
 
 export default {
   port: process.env.PORT || 4000,
-  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/invoice-ease',
+  mongoUri: process.env.MONGO_URI || 'mongodb+srv://zandi:zandi1201@cluster0.eyym5dq.mongodb.net/invoice-ease?retryWrites=true&w=majority',
   jwtSecret: process.env.JWT_SECRET || 'please-change-me',
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || '15m',
   refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN || '7d'
